2ab382c43b48e19c9630787bd4d063929c84c353
Jan Kryl <jan.kryl@nic.cz>
2006-04-07 10:39:50 +0000

    mod_whoisd: prace na corba rozhrani
    git-svn-id: file:///data/svn/enum/trunk/mod_eppd@127 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ epp_corba.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+#include "epp_corba.h"
+
+int corba_login(epp_data_login *login_data) {
+	login_data->svTRID = strdup("server-transID");
+	login_data->sessionID = 1010;
+	login_data->rc = 1000;
+
+	return 1;
+}
