0e1c5d84c9dc11bf5f388c878b7f5afaa826baa5
Ondřej Surý <ondrej.sury@nic.cz>
2006-11-30 18:19:16 +0000

    vypisovat start jako ERR je preci jenom osklive
    git-svn-id: file:///data/svn/enum/mod_eppd/trunk@2028 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- mod_eppd.c
+++ mod_eppd.c
@@ -1062,7 +1062,7 @@ static int epp_postconfig_hook(apr_pool_t *p, apr_pool_t *plog,
 		}
 		s = s->next;
 	}
-	ap_log_error(APLOG_MARK, APLOG_ERR, 0, s,
+	ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, s,
 		"mod_eppd started (mod_eppd version %s, SVN revision %s, BUILT %s %s)",
 				PACKAGE_VERSION, SVN_REV, __DATE__, __TIME__);
 
