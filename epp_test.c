be1cfdea7afbd77699c4bc908255962288c33199
Marina Shchavleva <marina.shchavleva@nic.cz>
2019-10-16 12:53:14 +0200

    Ticket #26157 - removed redundant definition of _GNU_SOURCE

--- epp_test.c
+++ epp_test.c
@@ -16,8 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with FRED.  If not, see <https://www.gnu.org/licenses/>.
  */
-#define _GNU_SOURCE
-
 #include "EPP.h"
 #include "epp-client.h"
 #include "epp_common.h"
